c9f3278cb9144c501b09c74e92d9853adc65c91a
Manish kumar Sinha <mksinha2011@gmail.com>
2018-07-13 22:11:36 +0530

    Update DropDownBoxController.java
    MK-3

--- src/main/java/com/mkyong/customer/controller/DropDownBoxController.java
+++ src/main/java/com/mkyong/customer/controller/DropDownBoxController.java
@@ -61,5 +61,10 @@ public class DropDownBoxController extends SimpleFormController{
 		return referenceData;
 		
 	}
+	public void addTwoNumber() {        
+      		int num1 = 5, num2 = 15, sum;
+      		sum = num1 + num2;
+	      System.out.println("Sum of these numbers: "+sum);
+	   }
 	
 }
